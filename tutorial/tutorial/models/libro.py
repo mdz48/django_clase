7c7d5f55020462770dd749450cdc361a2aa19a23
mdz48 <maximilianodiazclemen.cetis138@gmail.com>
2025-02-10 13:22:41 -0600

    Refactor Libro model to change autor field reference to Autor and update templates to remove navbar

--- tutorial/tutorial/models/libro.py
+++ tutorial/tutorial/models/libro.py
@@ -4,7 +4,7 @@ class Libro(models.Model):
     titulo = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True, null=True)
     fecha_publicacion = models.DateField()
-    autor = models.ForeignKey('Usuario', on_delete=models.CASCADE)
+    autor = models.ForeignKey('Autor', on_delete=models.CASCADE)
     categoria = models.ForeignKey(
         'Categoria', 
         on_delete=models.CASCADE,
